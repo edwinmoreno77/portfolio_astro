13eff81fe502e10b60eacb5a8bdd1ff18c34e69f
Edwin Moreno <edwinmoreno77@gmail.com>
2025-07-19 18:24:11 -0400

    site added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://edwinmoreno.dev/",
+  site: "https://github.com/edwinmoreno77/portfolio_astro",
 });
